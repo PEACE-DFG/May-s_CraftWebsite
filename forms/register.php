1d0e55719d7a841458adaa51396013b4d6da703f
PEACE-DFG <awofesobipeace@gmail.com>
2023-12-27 00:04:48 +0100

    converted to php files

---
+++ forms/register.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="stylesheet" href="styles.css">
+  <link href=" https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <div class="container">
+    <form>
+      <p>SIGN-UP</p>
+      <input type="email" placeholder="Email"><br>
+      <input type="password" placeholder="Password"><br>
+      <input type="text" placeholder="Phone Number"><br>
+      <input type="button" value="Sign in"><br>
+      <a href="#">Forgot Password?</a><br>
+      <small>Already have an account?</small>
+      <br>
+      <a href="login.php">Sign-in</a>
+    </form>
+
+    <div class="drops">
+      <div class="drop drop-1"></div>
+      <div class="drop drop-2"></div>
+      <div class="drop drop-3"></div>
+      <div class="drop drop-4"></div>
+      <div class="drop drop-5"></div>
+    </div>
+  </div>
+</body>
+
+</html>
